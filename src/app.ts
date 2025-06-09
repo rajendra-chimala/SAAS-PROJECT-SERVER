ff5be388c12f408f4732597683ba25930bcf32a8
rajendra-chimala <rajendrachimala5@gmail.com>
2025-06-09 18:09:40 +0545

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 import  {authRoutes}  from './routes/globals/auth/authRoute';
 
+app.use(express.json());
+app.use(cors())
 app.use("/api/auth", authRoutes);
 
 export default app;
--- src/controller/globals/authController.ts
+++ src/controller/globals/authController.ts
@@ -4,6 +4,7 @@
 
 import { Request, Response } from "express";
 import User from "../../database/models/user.model";
+import bcrypt from "bcrypt";
 
 export const registerUser = async (req: Request, res: Response) => {
 
@@ -11,10 +12,33 @@ export const registerUser = async (req: Request, res: Response) => {
     if(!username || !password || !email) {
         return res.status(400).json({ message: "Username, password and email are required." });
     }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
+        return res.status(400).json({ message: "User already exists." });
+    }
     await User.create({
         username,
-        password,
+        password: hashedPassword,
         email,
     })
     return res.status(201).json({ message: "User registered successfully." });
+}
+
+export const loginUser = async(req:Request, res:Response)=>{
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ message: "Email and password are required." });
+    }
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+        return res.status(401).json({ message: "Invalid email or password." });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid email or password." });
+    }
+    // Here you would typically generate a JWT token and send it back to the client
+    return res.status(200).json({ message: "Login successful." });
+
 }
